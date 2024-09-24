8496fc38e45b9aa25ddf6e0f045865d4ed337fe8
maxgatopv <maxga.p@topvalue.com>
2024-09-24 22:04:32 +0700

    index bottom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["./build/*.html"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#003a76',
+        'primaryDark': '#012143',
+      },
+      fontFamily: {
+        'sukhumvit': ['Sukhumvit', 'sans-serif'], // Define the custom font
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      const headings = {
+        'h1': '4xl',
+        'h2': '3xl',
+        'h3': '2xl',
+        'h4': 'xl',
+        'h5': 'lg'
+      };
+  
+      addBase(Object.fromEntries(
+        Object.entries(headings).map(([tag, size]) => [
+          tag, { fontSize: theme(`fontSize.${size}`), fontWeight: theme('fontWeight.bold') }
+        ])
+      ));
+    })
+  ]
 };
