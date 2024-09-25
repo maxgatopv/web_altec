524491b098b227198babf6f663f0c82992426614
maxga <maxga@maxgas-MacBook-Pro.local>
2024-09-25 17:57:37 +0700

    index and products done

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # web_altec
 npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
-
+npm run tailwind
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,63 @@ module.exports = {
       colors: {
         'primary': '#003a76',
         'primaryDark': '#012143',
+        'dark': '#141416',
       },
       fontFamily: {
         'sukhumvit': ['Sukhumvit', 'sans-serif'], // Define the custom font
       },
+    },
+    keyframes: {
+      slideUp: {
+        "0%": { transform: "translateY(20px)", opacity: "0" },
+        "100%": { transform: "translateY(0)", opacity: "1" },
+      },
+      slideDown: {
+        "0%": { transform: "translateY(-20px)", opacity: "0" },
+        "100%": { transform: "translateY(0)", opacity: "1" },
+      },
+      bgLeft: {
+        "0%": { backgroundPosition: "center" },
+        "100%": { backgroundPosition: "right" },
+      },
+      bounce: {
+        "0%, 100%": {
+          transform: "translateY(-20%)",
+          animationTimingFunction: "cubic-bezier(0, 0, 0, 0)",
+        },
+        "50%": {
+          transform: "translateY(0)",
+          animationTimingFunction: "cubic-bezier(0, 0, 0, 0)",
+        },
+      },
+      spin: {
+        from: { transform: "rotate(0deg)" },
+        to: { transform: "rotate(360deg)" },
+      },
+      ping: {
+        '75%, 100%': {
+          transform: "scale(2)",
+          opacity: "0",
+        }
+      },
+      pulse: {
+        '0%, 100%': {
+          opacity: '1',
+        },
+        '50%' : {
+          opacity: '.5',
+        }
+      },
+    },
+    animation: {
+      slideUp: "slideUp 0.5s ease-in-out forwards",
+      slideDown: "slideDown 0.5s ease-in-out forwards",
+      bgLeft: "bgLeft 0.5s ease-in-out forwards",
+      bounce: "bounce 3s infinite",
+      spin: "spin 1s linear infinite",
+      ping: "ping 1s ease-out infinite",
+      pulse: "pulse 2s cubic-bezier(0.4, 0, 0.5, 2) infinite",
+    
     },
   },
   plugins: [
